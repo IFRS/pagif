5ccf42f77c60989cb1161c3087e7c38902e94e7e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-05-19 16:29:34 -0300

    Ajustes na validação dos campos para o pagtesouro

--- api/controllers/pagtesouroController.js
+++ api/controllers/pagtesouroController.js
@@ -1,20 +1,85 @@
 const validator = require('express-validator');
 const pagtesouro = require('../pagtesouro');
 
+function sanitizeValor(valor) {
+  return valor.replace(',', '.');
+}
+
 module.exports.pagar = [
-  validator.body('codigoServico', '').notEmpty().isNumeric({ no_symbols: true }).isLength({ min: 1, max: 5 }),
-  validator.body('referencia', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 1, max: 20 }),
-  validator.body('competencia', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 6, max: 6 }).isDate({ format: 'MMYYYY' }),
-  validator.body('vencimento', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 8, max: 8 }).isDate({ format: 'DDMMYYYY' }),
-  validator.body('expiracaoPix', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 1, max: 2 }).isIn({ min: 1, max: 23 }),
-  validator.body('cnpjCpf', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 14, max: 14 }),
-  validator.body('nomeContribuinte', '').notEmpty().isAlphanumeric().isLength({ min: 1, max: 45 }),
-  validator.body('valorPrincipal', '').notEmpty().isNumeric({ no_symbols: true }).isLength({ min: 3, max: 17 }),
-  validator.body('valorDescontos', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 3, max: 17 }),
-  validator.body('valorOutrasDeducoes', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 3, max: 17 }),
-  validator.body('valorMulta', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 3, max: 17 }),
-  validator.body('valorJuros', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 3, max: 17 }),
-  validator.body('valorOutrosAcrescimos', '').optional().isNumeric({ no_symbols: true }).isLength({ min: 3, max: 17 }),
+  validator.body('codigoServico', '')
+    .notEmpty()
+    .trim()
+    .isNumeric({ no_symbols: true })
+    .isLength({ min: 1, max: 5 }),
+  validator.body('referencia', '')
+    .optional()
+    .trim()
+    .isNumeric({ no_symbols: true })
+    .isLength({ min: 1, max: 20 }),
+  validator.body('competencia', '')
+    .optional()
+    .trim()
+    .isNumeric({ no_symbols: true })
+    .isLength({ min: 6, max: 6 })
+    .isDate({ format: 'MMYYYY' }),
+  validator.body('vencimento', '')
+    .optional()
+    .trim()
+    .isNumeric({ no_symbols: true })
+    .isLength({ min: 8, max: 8 })
+    .isDate({ format: 'DDMMYYYY' }),
+  validator.body('expiracaoPix', '')
+    .optional()
+    .trim()
+    .isNumeric({ no_symbols: true })
+    .isLength({ min: 1, max: 2 })
+    .isIn({ min: 1, max: 23 }),
+  validator.body('cnpjCpf', '')
+    .optional()
+    .trim()
+    .isNumeric({ no_symbols: true })
+    .isLength({ min: 11, max: 14 }),
+  validator.body('nomeContribuinte', '')
+    .notEmpty()
+    .trim()
+    .isAlpha('pt-BR', { ignore: ' _-' })
+    .isLength({ min: 1, max: 45 }),
+  validator.body('valorPrincipal', '')
+    .notEmpty()
+    .trim()
+    .customSanitizer(sanitizeValor)
+    .isDecimal({ decimal_digits: 2 })
+    .isLength({ min: 4, max: 18 }),
+  validator.body('valorDescontos', '')
+    .optional()
+    .trim()
+    .customSanitizer(sanitizeValor)
+    .isDecimal({ decimal_digits: 2 })
+    .isLength({ min: 4, max: 18 }),
+  validator.body('valorOutrasDeducoes', '')
+    .optional()
+    .trim()
+    .customSanitizer(sanitizeValor)
+    .isDecimal({ decimal_digits: 2 })
+    .isLength({ min: 4, max: 18 }),
+  validator.body('valorMulta', '')
+    .optional()
+    .trim()
+    .customSanitizer(sanitizeValor)
+    .isDecimal({ decimal_digits: 2 })
+    .isLength({ min: 4, max: 18 }),
+  validator.body('valorJuros', '')
+    .optional()
+    .trim()
+    .customSanitizer(sanitizeValor)
+    .isDecimal({ decimal_digits: 2 })
+    .isLength({ min: 4, max: 18 }),
+  validator.body('valorOutrosAcrescimos', '')
+    .optional()
+    .trim()
+    .customSanitizer(sanitizeValor)
+    .isDecimal({ decimal_digits: 2 })
+    .isLength({ min: 4, max: 18 }),
   function(req, res) {
     const errors = validator.validationResult(req);
     if (!errors.isEmpty()) {
@@ -30,8 +95,7 @@ module.exports.pagar = [
       return res.json(response.data);
     })
     .catch((error) => {
-      console.error(error);
-      return res.json({});
+      return res.json(error);
     });
   }
 ];
