8b5036ace0fcb42ea26a152d622d5b9f046f2630
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-05-27 17:36:15 -0300

    Remoção de comentários desnecessários

--- api/routes/pagar.js
+++ api/routes/pagar.js
@@ -2,10 +2,8 @@ const { Router } = require('express');
 
 const router = Router();
 
-// Initialize Controller
 const pagtesouroController = require('../controllers/pagtesouroController');
 
-// Create
 router.post('/pagar', pagtesouroController.pagar);
 
 module.exports = router;
