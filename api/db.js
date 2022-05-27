40e6f9703939175400764b5a9636914349f2c364
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-05-27 17:34:47 -0300

    Uso do banco de dados

--- api/db.js
+++ api/db.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/ifpag', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+mongoose.connect('mongodb://localhost/ifpag');
+
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro na Conexão:'));
 db.once('open', function callback () {
