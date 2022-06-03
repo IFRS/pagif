a66bb2c151d244d88e4b8f374ce941918449ae6a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-06-03 10:14:53 -0300

    Criação do store para Unidades

---
+++ store/unidades.js
@@ -0,0 +1,35 @@
+export const state = () => ({
+  list: [],
+  item: {},
+})
+
+export const mutations = {
+  updateUnidades: (state, payload) => {
+    state.list = payload;
+  },
+  updateUnidade: (state, payload) => {
+    state.item = payload;
+  },
+  removeUnidade: (state, payload) => {
+    state.item = {};
+    state.list = state.list.filter((value) => {
+      return value._id !== payload._id;
+    });
+  },
+}
+
+export const actions = {
+  async save(context) {
+    return await this.$axios.post('/api/unidades', context.state.item)
+    .then((response) => {
+      context.commit('addUnidade', response.data);
+      context.commit('updateUnidade', {});
+    });
+  },
+  async delete(context) {
+    return await this.$axios.delete('/api/unidades/' + context.state.item._id)
+    .then((response) => {
+      context.commit('removeUnidade', response.data);
+    });
+  },
+}
