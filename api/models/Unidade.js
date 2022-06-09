86d333c7ab3f4987d7e978cbdd7d38a0a5316f37
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-06-09 15:13:42 -0300

    Adição de timestamps para Unidades

--- api/models/Unidade.js
+++ api/models/Unidade.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Unidade = new Schema ({
+const Unidade = new Schema({
   nome: { type: String, required: true },
   slug: { type: String, required: true, index: { unique: true } },
   token: { type: String, required: true, index: { unique: true } },
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Unidade', Unidade);
