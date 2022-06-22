fffc0d027b99ff13c1e711a678820e5687fea5aa
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-06-22 12:02:23 -0300

    Correção de typo

--- api/models/Servico.js
+++ api/models/Servico.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const Servico = new Schema({
   unidade: { type: Schema.Types.ObjectId, ref: 'Unidade' },
-  codigo: {type: Number, required: true },
+  codigo: { type: Number, required: true },
   nome: { type: String, required: true },
   desc: { type: String },
   vencimentoDias: { type: Number },
