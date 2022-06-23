b06f69ef2b85e3c905a60df0fc4cc013ed9bf672
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-06-23 14:59:52 -0300

    Transformação dos dados para o Pagtesouro na instância do axios

--- api/pagtesouro.js
+++ api/pagtesouro.js
@@ -1,10 +1,99 @@
 const axios = require('axios');
+const dayjs = require('dayjs');
+
+function valor_formatter(valor) {
+  valor_formatted = String(valor).padStart(3, '0');
+  valor_formatted = valor_formatted.split('');
+  valor_formatted.splice(-2, 0, '.');
+  valor_formatted = valor_formatted.join('');
+  return valor_formatted;
+}
 
 module.exports = axios.create({
   baseURL: process.env.PAGTESOURO_URL,
-  headers: {
-    common: {
-      'Authorization': `Bearer ${process.env.PAGTESOURO_TOKEN}`,
-    },
-  },
+  transformRequest: [
+    (data, headers) => {
+      if (data.hasOwnProperty('competencia') && data.competencia) {
+        let competencia = dayjs(data.competencia).format('MMYYYY');
+        if (competencia) {
+          data.competencia = competencia;
+        } else {
+          data.competencia = null;
+        }
+      }
+
+      if (data.hasOwnProperty('vencimento') && data.vencimento) {
+        let vencimento = dayjs(data.vencimento).format('DDMMYYYY');
+        if (vencimento) {
+          data.vencimento = vencimento;
+        } else {
+          data.vencimento = null;
+        }
+      }
+
+      if (data.hasOwnProperty('valorPrincipal') && data.valorPrincipal) {
+        let valorPrincipal = valor_formatter(data.valorPrincipal);
+
+        if (valorPrincipal) {
+          data.valorPrincipal = valorPrincipal;
+        } else {
+          data.valorPrincipal = null;
+        }
+      }
+
+      if (data.hasOwnProperty('valorDescontos') && data.valorDescontos) {
+        let valorDescontos = valor_formatter(data.valorDescontos);
+
+        if (valorDescontos) {
+          data.valorDescontos = valorDescontos;
+        } else {
+          data.valorDescontos = null;
+        }
+      }
+
+      if (data.hasOwnProperty('valorOutrasDeducoes') && data.valorOutrasDeducoes) {
+        let valorOutrasDeducoes = valor_formatter(data.valorOutrasDeducoes);
+
+        if (valorOutrasDeducoes) {
+          data.valorOutrasDeducoes = valorOutrasDeducoes;
+        } else {
+          data.valorOutrasDeducoes = null;
+        }
+      }
+
+      if (data.hasOwnProperty('valorMulta') && data.valorMulta) {
+        let valorMulta = valor_formatter(data.valorMulta);
+
+        if (valorMulta) {
+          data.valorMulta = valorMulta;
+        } else {
+          data.valorMulta = null;
+        }
+      }
+
+      if (data.hasOwnProperty('valorJuros') && data.valorJuros) {
+        let valorJuros = valor_formatter(data.valorJuros);
+
+        if (valorJuros) {
+          data.valorJuros = valorJuros;
+        } else {
+          data.valorJuros = null;
+        }
+      }
+
+      if (data.hasOwnProperty('valorOutrosAcrescimos') && data.valorOutrosAcrescimos) {
+        let valorOutrosAcrescimos = valor_formatter(data.valorOutrosAcrescimos);
+
+        if (valorOutrosAcrescimos) {
+          data.valorOutrosAcrescimos = valorOutrosAcrescimos;
+        } else {
+          data.valorOutrosAcrescimos = null;
+        }
+      }
+
+      data.urlRetorno = process.env.SERVER_BASE_URL + '/';
+
+      return data;
+    }
+  ].concat(axios.defaults.transformRequest),
 });
