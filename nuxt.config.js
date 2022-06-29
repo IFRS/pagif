705a357ed3e23c5c5c4af49d20e79f055b1c4ae8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-06-29 13:33:00 -0300

    Adição dos arquivos da API no watch do Nuxt e ajustes gerais

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,20 @@ require('dotenv').config();
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - ifpag',
-    title: 'ifpag',
+    titleTemplate: '%s - IFPAG',
+    title: 'IFPAG',
     htmlAttrs: {
-      lang: 'pt-BR'
+      lang: 'pt-BR',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Sistema de Pagamentos integrado ao PagTesouro.' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -80,5 +80,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    watch: [
+      '~/api/',
+    ],
+  },
 }
