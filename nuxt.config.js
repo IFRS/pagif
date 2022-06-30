e080c144b169c27f7af3efa570c98c84f4dcc8f2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-06-30 15:01:11 -0300

    Adição de plugins para lidar com datas e formatações em geral

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/toast.js',
+    '~/plugins/dayjs.js',
+    '~/plugins/format.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/dayjs.js
@@ -0,0 +1,3 @@
+export default ({ app, store }, inject) => {
+  inject('dayjs', require('dayjs'));
+};
