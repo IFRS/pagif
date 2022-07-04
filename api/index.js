abc666d8f244219a64b1be9529d9bcb1435b03d9
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-07-04 17:13:26 -0300

    Uso da biblioteca CORS para controle de requisições à API

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 const db = require('./db');
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 
-// Create express instance
 const app = express();
 
-// Init body-parser options (inbuilt with express)
+
+app.use(cors({
+  origin: process.env.BROWSER_BASE_URL,
+  optionsSuccessStatus: 200,
+}));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
 
-// Require & Import API routes
 const unidades = require('./routes/unidades');
 const servicos = require('./routes/servicos');
 const pagamentos = require('./routes/pagamentos');
@@ -21,13 +24,11 @@ if (process.env.NODE_ENV === 'development') {
   app.use((req, res, next) => setTimeout(next, 999));
 }
 
-// Use API Routes
 app.use(unidades);
 app.use(servicos);
 app.use(pagamentos);
 app.use(notifica);
 
-// Export the server middleware
 module.exports = {
   path: '/api',
   handler: app,
