ce484c54237dd3d744d755724210389fd73ac454
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-07-08 14:56:14 -0300

    Preenchimento parcial da etapa de resumo do pagamento

--- components/Pagamento/Resumo.vue
+++ components/Pagamento/Resumo.vue
@@ -7,7 +7,14 @@
     </v-row>
     <v-row>
       <v-col>
-
+        <p>
+          Voc&ecirc; pagar&aacute; a quantia de <strong>R$ {{ $format.intToMoeda($store.getters['pagamento/valorPrincipal']) }}</strong> para <strong>{{ $store.getters['unidade/nome'] }}</strong> pelo produto/servi&ccedil;o <strong>"{{ $store.getters['pagamento/codigoServico'] }}"</strong>.
+        </p>
+        <p>
+          O pagamento ser&aacute; em nome de <strong>{{ $store.getters['pagamento/nomeContribuinte'] }}<template v-if="$store.getters['pagamento/cnpjCpf']">&nbsp;({{ $format.cnpjCpf($store.getters['pagamento/cnpjCpf']) }})</template></strong>.
+        </p>
+        <br>
+        <p class="text-body-2 info--text">Ao clicar em "Concluir" o pagamento ser&aacute; gerado e voc&ecirc; ter&aacute; <strong>24h</strong> para realiz&aacute;-lo.</p>
       </v-col>
       <v-col>
 
