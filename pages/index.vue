abb7f540f8a746317d02b9d0750f150845535ad8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-08-04 16:20:35 -0300

    Criação de página inicial com links para pagamento e consulta

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <h2 class="mb-5 text-center">Sistema de Pagamentos</h2>
+    <v-row class="text-center">
+      <v-col>
+        <v-btn
+          color="secondary"
+          :to="{ name: 'consulta' }"
+          x-large
+        >
+          Consultar Pagamento
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn
+        color="primary"
+          :to="{ name: 'pagamento' }"
+          x-large
+        >
+          Realizar Pagamento
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
