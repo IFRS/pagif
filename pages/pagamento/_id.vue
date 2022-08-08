012794dc997a03b7e2c5a164509a5bddb401a77b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-08-08 17:23:19 -0300

    Adição de verificação e botão para copiar código do pagamento

--- pages/pagamento/_id.vue
+++ pages/pagamento/_id.vue
@@ -8,6 +8,14 @@
         Pague agora ou salve o Código do Pagamento abaixo para pagar em outro momento.
       </v-card-text>
       <v-list-item two-line>
+        <v-list-item-action>
+          <v-btn
+            icon
+            @click="copy($store.getters['pagamento/idPagamento'])"
+          >
+            <v-icon>mdi-content-copy</v-icon>
+          </v-btn>
+        </v-list-item-action>
         <v-list-item-content>
           <v-list-item-title>{{ $store.getters['pagamento/idPagamento'] }}</v-list-item-title>
           <v-list-item-subtitle>Código do Pagamento</v-list-item-subtitle>
@@ -20,6 +28,7 @@
         >
           <template v-slot:activator="{ on, attrs }">
             <v-btn
+              class="mr-5"
               text
               v-bind="attrs"
               v-on="on"
@@ -36,7 +45,7 @@
             </v-card-title>
 
             <v-card-text>
-              Lembre-se de anotar o Código do Pagamento para acesso posterior. Sem esse código <strong>não</strong> é possível realizar esse pagamento.
+              Lembre-se de anotar o Código do Pagamento para acesso posterior. Sem esse código <strong>não</strong> é possível retomar esse pagamento.
             </v-card-text>
 
             <v-divider></v-divider>
@@ -46,20 +55,53 @@
               <v-btn
                 color="primary"
                 text
-                @click="$router.push({name: 'pagamento' })"
+                :to="{ name: 'index' }"
               >
-                Estou Ciente, Prosseguir
+                Estou ciente, quero sair dessa tela
               </v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
-        <v-btn
-          text
-          color="primary"
-          @click="$router.push({name: 'pagar-id', params: { id: $store.getters['pagamento/idPagamento'] } })"
+        <v-dialog
+          v-model="pagarDialog"
+          width="500"
         >
-          Pagar Agora
-        </v-btn>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              text
+              color="primary"
+            >
+              Pagar Agora
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title>
+              Atenção!
+              <v-spacer></v-spacer>
+              <v-icon @click="pagarDialog = false;">mdi-close</v-icon>
+            </v-card-title>
+
+            <v-card-text>
+              Ao iniciar o pagamento <strong>não</strong> será mais possível voltar em outro momento.
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                :to="{ name: 'pagar-id', params: { id: $store.getters['pagamento/idPagamento'] } }"
+              >
+                Estou ciente, prosseguir para o pagamento
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-card-actions>
     </v-card>
   </v-container>
@@ -71,6 +113,7 @@ export default {
   data() {
     return {
       voltarDialog: false,
+      pagarDialog: false,
     }
   },
   async fetch() {
@@ -79,6 +122,18 @@ export default {
       this.$toast.error('Ocorreu um erro ao buscar o Pagamento. ' + error.message);
       console.error(error);
     })
+  },
+  methods: {
+    copy(text) {
+      if (Navigator.clipboard) {
+        Navigator.clipboard.writeText(text)
+        .then(() => {
+          this.$toast.success('Código do Pagamento copiado com sucesso!');
+        });
+      } else {
+        this.$toast.info('Seu navegador não suporta esse recurso.');
+      }
+    }
   },
   destroyed () {
     this.$store.commit('pagamento/clear');
