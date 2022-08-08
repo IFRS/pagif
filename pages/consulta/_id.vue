12aadd976a33fec32a840eb81392bbaf2cbe7bb3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-08-08 10:20:12 -0300

    Criação da página de consulta de pagamento

---
+++ pages/consulta/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <pagamento-detalhes>
+      <v-btn
+        color="primary"
+        plain
+        :to="{ name: 'consulta' }"
+      >Voltar</v-btn>
+    </pagamento-detalhes>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ConsultaID',
+  async fetch() {
+    await this.$store.dispatch('pagamento/show', this.$route.params.id)
+    .catch((error) => {
+      this.$toast.error('Ocorreu um erro ao buscar o Pagamento. ' + error.message);
+      console.error(error);
+    })
+  },
+  destroyed() {
+    this.$store.commit('pagamento/clear');
+  },
+}
+</script>
--- pages/consulta/index.vue
+++ pages/consulta/index.vue
@@ -1,9 +1,63 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <h2>Consultar Pagamento</h2>
+
+    <v-form
+      ref="form"
+      lazy-validation
+      @submit.prevent="submitConsulta"
+    >
+      <v-row dense>
+        <v-col>
+          <v-text-field
+            v-model="idPagamento"
+            label="Código do Pagamento"
+            hint="Insira um código de pagamento em andamento."
+            clearable
+            filled
+            :rules="validation"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn
+            type="submit"
+            icon
+            x-large
+          >
+            <v-icon>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'Consulta',
+  head: {
+    title: 'Consulta Pagamento',
+  },
+  data() {
+    return {
+      idPagamento: null,
+      validation: [],
+    }
+  },
+  methods: {
+    submitConsulta() {
+      this.validation = [
+        v => !!v || 'Código do Pagamento é obrigatório.',
+        v => (/^[A-Za-z0-9]+$/).test(v) || 'Código do Pagamento só pode conter letras e números.',
+      ];
+
+      this.$nextTick(() => {
+        if (this.$refs.form.validate()) {
+          this.$router.push({ name: 'consulta-id', params: { id: this.idPagamento } });
+        }
+      });
+    },
+  },
 }
 </script>
