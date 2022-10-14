6b46abcc41b9258e88e67e667ce430f57795f955
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-10-14 17:31:40 -0300

    Ajuste na conexão com o banco para usar autenticação

--- api/db.js
+++ api/db.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+let db = null;
 
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro na Conexão:'));
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`).then(
+  () => {
+    db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Erro no MongoDB:'));
+  },
+  err => {
+    console.error('Erro ao tentar conectar: ' + err);
+  }
+);
 
 module.exports = db;
