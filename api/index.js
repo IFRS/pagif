938ea2be97a2847b13ba5ffc30adf8a6d441df5e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-10-26 08:44:09 -0300

    Definição de expiração para a sessão

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
 require('../db');
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const passport = require('passport');
 
 const app = express();
+
+const expire = (process.env.NODE_ENV === 'development') ? 1000 * 60 * 60 * 24 * 7 : 1000 * 60 * 60 * 1;
 
 let session_store = new MongoDBStore({
   uri: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   databaseName: process.env.DB_NAME,
   collection: 'sessions',
+  expires: expire,
 });
 session_store.on('error', function(error) {
   console.error(error);
@@ -29,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7,
+    maxAge: expire,
   },
   store: session_store,
   resave: true,
