be112135dcc9a8c073da0a0b78c5794deeb92ac3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-10-26 09:08:37 -0300

    Remoção da fonte Rawline por problemas na altura da fonte

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,6 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/transitions.scss',
-    '~/assets/rawline.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
