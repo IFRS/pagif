b9f695b6797e1ee1bccdf71ab80141b95a7047f9
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-10-27 17:29:48 -0300

    Refatoração na conexão com o MongoDB

--- db/index.js
+++ db/index.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
-
-let db = null;
+const mongoose = require('mongoose');
 
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`).then(
   () => {
-    db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'Erro no MongoDB:'));
+    mongoose.connection
+    .on('error', console.error.bind(console, 'Erro no MongoDB:'));
   },
   err => {
-    console.error('Erro ao tentar conectar: ' + err);
+    console.error('Erro ao tentar conectar no MongoDB: ' + err);
   }
 );
 
-module.exports = db;
+module.exports = mongoose.connection;
