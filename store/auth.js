655d58cb26afc1f869a48d0d7c443e89ba1e1a0f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-11-04 17:15:59 -0300

    Correção no algoritmo de verificação de role do usuário

--- store/auth.js
+++ store/auth.js
@@ -5,11 +5,10 @@ export const state = () => ({
 export const getters = {
   user: state => state.user,
   userHasRole: (state) => (roles) => {
-    const rolesArray = Array.of(roles);
+    const rolesArray = Array.isArray(roles) ? roles : Array.of(roles);
     if (state.user.superadmin) return true;
     if (!state.user.roles || state.user.roles.length === 0) return false;
     return state.user.roles.some((role) => {
-      // return role.tipo === value;
       return rolesArray.includes(role.tipo);
     });
   },
