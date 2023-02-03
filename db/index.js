62fff0019423c7ed6f7dd47db4eee7d718d7250b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-02-03 11:26:12 -0300

    Definição de opção requerida pelo Mongoose

--- db/index.js
+++ db/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 mongoose.plugin(accessibleRecordsPlugin);
 
+mongoose.set('strictQuery', true);
+
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`).then(
   () => {
     mongoose.connection
