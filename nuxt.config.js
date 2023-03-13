683746acc232aa3452f01e33491e80ce0dae7442
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-03-13 15:09:14 -0300

    Ajuste no nome do sistema

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - PAGIF',
+    titleTemplate: '%s - PagIF',
     title: 'Sistema de Pagamentos',
     htmlAttrs: {
       lang: 'pt-BR',
