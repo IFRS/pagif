709732be99be23bbea246a7445a0adf197af7e30
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-05-22 16:32:53 -0300

    Tentativa de refatoração do Index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -75,33 +75,33 @@
   </v-container>
 </template>
 
-<script>
-import { mapGetters, mapMutations } from 'vuex';
+<script setup>
+import { onBeforeMount } from 'vue';
+import { useRoute, useRouter } from 'nuxt/app';
+import { storeToRefs } from 'pinia';
+import { useMainStore } from '~/store';
+import { useConfigStore } from '~/store/config';
 
-export default {
-  computed: {
-    intro: {
-      ...mapGetters({ get: 'config/intro' }),
-      ...mapMutations({ set: 'config/intro' }),
-    }
-  },
-  beforeCreate() {
-    if (this.$route.query.unidade) {
-      const unidade = this.$store.getters['unidades'].find(unidade => (unidade.slug && unidade.slug === this.$route.query.unidade));
+const mainStore = useMainStore();
+const configStore = useConfigStore();
+const { intro } = storeToRefs(configStore);
+
+const route = useRoute();
+const router = useRouter();
+onBeforeMount(() => {
+  if (route.query.unidade) {
+      const unidade = mainStore.unidades.find(unidade => (unidade.slug && unidade.slug === route.query.unidade));
       if (unidade) {
-        this.$store.commit('config/unidade', unidade);
+        configStore.unidade = unidade;
         this.$toast.success(`Unidade definida para "${unidade.nome}".`);
       }
 
-      this.$router.replace({ 'query': null });
+      router.replace({ 'query': null });
     }
-  },
-}
+});
 </script>
 
 <style lang="scss" scoped>
-@import '~vuetify/src/styles/styles.sass';
-
 .intro {
   display: flex;
   flex-direction: row;
@@ -110,9 +110,9 @@ export default {
   justify-content: center;
   gap: 1rem;
 
-  @media #{map-get($display-breakpoints, 'sm-and-down')} {
-    flex-direction: column;
-  }
+  // @media #{map-get($display-breakpoints, 'sm-and-down')} {
+  //   flex-direction: column;
+  // }
 
   &__marca {
     flex: 0 1 160px;
@@ -121,9 +121,9 @@ export default {
   &__text {
     flex: 0 1 80ch;
 
-    @media #{map-get($display-breakpoints, 'sm-and-down')} {
-      flex-basis: auto;
-    }
+    // @media #{map-get($display-breakpoints, 'sm-and-down')} {
+    //   flex-basis: auto;
+    // }
   }
 }
 </style>
