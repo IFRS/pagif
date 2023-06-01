9e370ae412e4cfdd0d2b65768a640784a886272f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-06-01 10:23:49 -0300

    Refatoração do componente para exibir a situação do pagamento

--- frontend/components/Pagamento/Situacao.vue
+++ frontend/components/Pagamento/Situacao.vue
@@ -1,44 +1,42 @@
 <template>
   <span
     class="d-inline-block pa-1 font-weight-medium text-white"
-    :class="color(situacao)"
+    :class="color"
   ><slot>{{ situacao }}</slot></span>
 </template>
 
-<script>
-export default {
-  props: {
-    situacao: {
-      type: String,
-      default: '',
-    },
+<script setup>
+
+const props = defineProps({
+  situacao: {
+    type: String,
+    default: '',
   },
-  methods: {
-    color(situacao) {
-      switch (situacao) {
-        case 'CRIADO':
-          return 'blue accent-2';
-        break;
-        case 'INICIADO':
-          return 'blue accent-3';
-        break;
-        case 'SUBMETIDO':
-          return 'blue accent-4';
-        break;
-        case 'CONCLUIDO':
-          return 'light-green darken-2';
-        break;
-        case 'REJEITADO':
-          return 'red darken-3';
-        break;
-        case 'CANCELADO':
-          return 'deep-orange darken-3';
-        break;
-        default:
-          return '';
-        break;
-      }
-    },
-  },
-}
+})
+
+const color = computed(() => {
+  switch (props.situacao) {
+    case 'CRIADO':
+      return 'bg-blue-accent-2';
+    break;
+    case 'INICIADO':
+      return 'bg-blue-accent-3';
+    break;
+    case 'SUBMETIDO':
+      return 'bg-blue-accent-4';
+    break;
+    case 'CONCLUIDO':
+      return 'bg-light-green-darken-2';
+    break;
+    case 'REJEITADO':
+      return 'bg-red-darken-3';
+    break;
+    case 'CANCELADO':
+      return 'bg-deep-orange-darken-3';
+    break;
+    default:
+      return 'bg-black';
+    break;
+  }
+})
 </script>
