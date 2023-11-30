b8234bfe2a5e44832b7d26460400eea811290ddb
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-11-30 10:04:10 -0300

    Loading do index usando skeleton loader

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -74,79 +74,11 @@
         </v-col>
       </v-row>
       <template #fallback>
-        <div
-          class="d-flex justify-center align-center"
-          style="height: 50dvh;"
-        >
-          <svg
-            width="200"
-            height="200"
-            viewBox="0 0 38 38"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <linearGradient
-                id="a"
-                x1="8.042%"
-                y1="0%"
-                x2="65.682%"
-                y2="23.865%"
-              >
-                <stop
-                  stop-color="#62C65E"
-                  stop-opacity="0"
-                  offset="0%"
-                />
-                <stop
-                  stop-color="#62C65E"
-                  stop-opacity=".631"
-                  offset="63.146%"
-                />
-                <stop
-                  stop-color="#62C65E"
-                  offset="100%"
-                />
-              </linearGradient>
-            </defs>
-            <g
-              fill="none"
-              fill-rule="evenodd"
-            >
-              <g transform="translate(1 1)">
-                <path
-                  id="Oval-2"
-                  d="M36 18c0-9.94-8.06-18-18-18"
-                  stroke="url(#a)"
-                  stroke-width="2"
-                >
-                  <animateTransform
-                    attributeName="transform"
-                    type="rotate"
-                    from="0 18 18"
-                    to="360 18 18"
-                    dur="0.9s"
-                    repeatCount="indefinite"
-                  />
-                </path>
-                <circle
-                  fill="#5EB152"
-                  cx="36"
-                  cy="18"
-                  r="1"
-                >
-                  <animateTransform
-                    attributeName="transform"
-                    type="rotate"
-                    from="0 18 18"
-                    to="360 18 18"
-                    dur="0.9s"
-                    repeatCount="indefinite"
-                  />
-                </circle>
-              </g>
-            </g>
-          </svg>
-        </div>
+        <v-skeleton-loader
+          type="paragraph@2, actions"
+          class="loader"
+          max-width="80ch"
+        />
       </template>
     </ClientOnly>
   </v-container>
@@ -201,5 +133,9 @@ const { smAndDown } = useDisplay();
   &__text {
     flex: 0 1 v-bind('smAndDown ? "auto" : "80ch"');
   }
+}
+
+.loader {
+  margin: 75px auto;
 }
 </style>
