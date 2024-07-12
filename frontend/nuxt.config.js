bb4d4f608fd481a695755f7890563cff8c652c73
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-07-12 14:25:12 -0300

    Adição de nova config recomendada pelo Nuxt

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
+  compatibilityDate: '2024-07-01',
   app: {
     head: {
       htmlAttrs: {
