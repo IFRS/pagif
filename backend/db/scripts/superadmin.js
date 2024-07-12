23442be7c159f791b5c36d1cbec59d6a180c1c0d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-07-12 13:38:03 -0300

    Refatoração na assinatura do findAndUpdate

--- backend/db/scripts/superadmin.js
+++ backend/db/scripts/superadmin.js
@@ -4,14 +4,17 @@ const args = process.argv.slice(2);
 
 const email = args[0];
 
-Usuario.findOneAndUpdate({ email: email }, { email: email, abilities: [{ action: 'manage', subject: 'all' }] }, { upsert: true }, async function(err, usuario) {
-  if (err) console.error('Erro ao buscar ou adicionar um Superadmin: ', err)
-
+Usuario.findOneAndUpdate({ email: email }, { email: email, abilities: [{ action: 'manage', subject: 'all' }] }, { upsert: true })
+.then(usuario => {
   if (usuario === null) {
     console.log('Super Admin criado com sucesso!');
   } else {
     console.error('Super Admin já cadastrado.');
   }
-
-  await DB.close();
+})
+.catch(err => {
+  console.error('Erro ao buscar ou adicionar um Superadmin: ', err);
+})
+.finally(() => {
+  DB.close();
 });
