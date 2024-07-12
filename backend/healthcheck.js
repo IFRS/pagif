6a4a68de7042dbb5989519bc156d2c95233306d2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-07-12 10:05:55 -0300

    Melhorias nos scripts de healthcheck para Docker

--- backend/healthcheck.js
+++ backend/healthcheck.js
@@ -1,22 +1,23 @@
-const http = require("http");
+const process = require('node:process');
+const http = require('http');
 require('dotenv').config();
 
 const options = {
-  host : "localhost",
-  port : process.env.LISTEN_PORT || "3000",
+  host : 'localhost',
+  port : process.env.LISTEN_PORT || '3030',
   timeout : 5000,
 };
 
 let request = http.request({ ...options, path: '/health' }, (res) => {
   console.log(`STATUS Geral: ${res.statusCode}`);
   if (res.statusCode !== 200) {
-    process.exit(1);
+    process.exitCode = 1;
   }
 });
 
 request.on('error', function(err) {
   console.error(`ERROR: ${err}`);
-  process.exit(1);
+  process.exitCode = 1;
 });
 
 request.end();
