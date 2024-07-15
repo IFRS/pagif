ceb6455fe2572775be5f142bfc076aff674019ec
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-07-15 16:06:35 -0300

    Estilização dos títulos das páginas

--- frontend/components/PageTitle.vue
+++ frontend/components/PageTitle.vue
@@ -1,13 +1,15 @@
 <template>
   <Component
     :is="tag"
-    class="mb-5"
+    class="titulo"
   >
     <slot />
   </Component>
 </template>
 
 <script setup>
+import colors from 'vuetify/util/colors'
+
 const props = defineProps({
   level: {
     default: '2',
@@ -19,3 +21,23 @@ const props = defineProps({
 
 const tag = computed(() => ('h' + props.level))
 </script>
+
+<style lang="scss" scoped>
+.titulo {
+  color: v-bind('colors.grey.darken4');
+  margin-bottom: 2rem;
+  padding-inline-start: 0.75rem;
+  position: relative;
+
+  &::before {
+    $size: 4px;
+
+    content: '';
+    position: absolute; left: 0; top: 50%; transform: translateY(-50%);
+    width: $size;
+    height: 70%;
+    background-color: rgb(var(--v-theme-pagif));
+    border-radius: calc($size / 2);
+  }
+}
+</style>
