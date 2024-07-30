e32af0deb6b7c2e132bef197f7dbd7cb98a0e706
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-07-30 09:56:10 -0300

    Migração para ESLint 9 e adição de estilizador de código

---
+++ backend/eslint.config.js
@@ -0,0 +1,25 @@
+const globals = require('globals');
+const nodePlugin = require('eslint-plugin-n');
+const js = require('@eslint/js');
+const stylistic = require('@stylistic/eslint-plugin');
+
+module.exports = [
+  stylistic.configs.customize({
+    semi: true,
+    jsx: false,
+    braceStyle: '1tbs',
+  }),
+  js.configs.recommended,
+  nodePlugin.configs['flat/recommended-script'],
+  {
+    name: 'Backend',
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: {
+        ...globals.commonjs,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+  },
+];
