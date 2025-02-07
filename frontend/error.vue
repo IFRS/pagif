1122979a9645643fab48de4102556f1b220755cc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-02-07 09:57:22 -0300

    Refatoração da página de erro

--- frontend/error.vue
+++ frontend/error.vue
@@ -1,41 +1,46 @@
 <template>
-  <v-app dark>
-    <v-container>
-      <v-alert
-        v-if="error.statusCode === 404"
-        elevation="5"
-        prominent
-        type="warning"
-        class="mb-5"
-      >
-        P&aacute;gina n&atilde;o encontrada!
-      </v-alert>
+  <NuxtLayout :name="layout">
+    <v-app dark>
+      <v-container>
+        <v-empty-state
+          :headline="mensagens.headline"
+          :title="mensagens.title"
+          :text="mensagens.text"
+          :image="mensagens.image"
+          :action-text="mensagens.actionText"
+          @click:action="handleError"
+        />
 
-      <v-alert
-        v-else
-        elevation="5"
-        prominent
-        type="error"
-        class="mb-5"
-      >
-        Ocorreu um erro inesperado.
-      </v-alert>
+        <DevOnly>
+          <v-card
+            v-if="error.message"
+            density="comfortable"
+            color="info"
+            variant="tonal"
+            title="Error Message"
+            class="mb-5 mx-auto w-lg-50"
+          >
+            <v-card-text>
+              <div v-html="error.message" />
+            </v-card-text>
+          </v-card>
 
-      <div
-        class="mb-5"
-        v-html="error.message"
-      />
-
-      <div
-        class="mb-5"
-        v-html="error.stack"
-      />
-
-      <v-btn @click="handleError">
-        Ir para a P&aacute;gina Inicial
-      </v-btn>
-    </v-container>
-  </v-app>
+          <v-card
+            v-if="error.stack"
+            density="comfortable"
+            color="info"
+            variant="tonal"
+            title="Error Stack"
+            class="mb-5 mx-auto w-lg-50"
+          >
+            <v-card-text>
+              <pre v-html="error.stack" />
+            </v-card-text>
+          </v-card>
+        </DevOnly>
+      </v-container>
+    </v-app>
+  </NuxtLayout>
 </template>
 
 <script setup>
@@ -46,12 +51,36 @@ const props = defineProps({
   },
 })
 
+const route = useRoute()
+
+const layout = computed(() => {
+  return route.name?.startsWith('admin') ? 'admin' : 'default'
+})
+
+const mensagens = ref({
+  headline: 'Erro',
+  title: 'Ocorreu um erro inesperado.',
+  text: 'Por favor, tente novamente mais tarde.',
+  image: '/img/errors/error.png',
+  actionText: 'Ir para a Página Inicial',
+})
+
 const authStore = useAuthStore()
 
-if (props.error?.statusCode == 401) {
-  authStore.$reset()
-  clearError({ redirect: '/' })
-}
+watch(() => props.error, () => {
+  if (props.error?.statusCode == 401) {
+    authStore.$reset()
+    clearError({ redirect: '/' })
+  } else if (props.error?.statusCode == 404) {
+    mensagens.value = {
+      headline: 'Erro 404',
+      title: 'Página não encontrada!',
+      text: 'Por favor, verifique o endereço e tente novamente. Ou clique no botão abaixo para voltar para a página inicial.',
+      image: '/img/errors/404.png',
+      actionText: 'Ir para a Página Inicial',
+    }
+  }
+}, { immediate: true })
 
 const handleError = () => clearError({ redirect: '/' })
 </script>
