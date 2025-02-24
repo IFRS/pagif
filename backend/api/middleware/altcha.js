fb893e6b86b3d38bff40f1aa332c0c9b379b26f5
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-02-24 17:46:12 -0300

    Correção no middleware para verificação do altcha

--- backend/api/middleware/altcha.js
+++ backend/api/middleware/altcha.js
@@ -3,13 +3,12 @@ import { logger } from '../../logger/index.js';
 
 export default function (req, res, next) {
   const hmacKey = process.env.ALTCHA_HMAC_KEY;
-  const payload = req.body.payload;
+  const payload = req.body.captcha;
 
   verifySolution(payload, hmacKey)
+    .then(() => next())
     .catch((altchaError) => {
       logger.warning('[ALTCHA] Invalid solution: %o', altchaError);
       return res.status(422).end();
     });
-
-  return next();
 };
