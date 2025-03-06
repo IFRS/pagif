955e7ed3b2cd17610b5d60738884e7e5b0a30a21
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-06 16:38:25 -0300

    Ajustes nos sinais de término do sub-processo das tarefas do backend

--- backend/queue/process.js
+++ backend/queue/process.js
@@ -1,6 +1,6 @@
-import { logger } from '../logger/index.js';
 import process from 'node:process';
 import pulse from './index.js';
+import { logger } from '../logger/index.js';
 
 (async function () {
   logger.info(`[Fila] Início do subprocesso com PID ${process.pid}.`);
@@ -12,8 +12,11 @@ async function graceful(code = 0) {
   process.exitCode = code;
 }
 
-process.on('SIGTERM', graceful);
-process.on('SIGINT', graceful);
+process.on('SIGHUP', graceful(0));
+process.on('SIGINT', graceful(0));
+process.on('SIGKILL', graceful(0));
+process.on('SIGTERM', graceful(0));
+
 process.on('unhandledRejection', (reason, promise) => {
   logger.error(`[Fila] Erro não tratado em: ${promise} Razão: ${reason}`);
   graceful(1);
