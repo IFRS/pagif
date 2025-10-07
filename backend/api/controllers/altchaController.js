1d8a4a6fb55cb3ef3e8ba03dc551ff44f09781a1
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-10-07 17:37:17 -0300

    Correção no uso da chave para o ALTCHA

--- backend/api/controllers/altchaController.js
+++ backend/api/controllers/altchaController.js
@@ -1,8 +1,8 @@
 import { createChallenge } from 'altcha-lib';
 
-const hmacKey = process.env.ALTCHA_HMAC_KEY;
-
 export const challenge = async (req, res) => {
+  const hmacKey = process.env.ALTCHA_HMAC_KEY;
+
   const challenge = await createChallenge({
     hmacKey,
     maxNumber: 1_000_000,
